069b1014cc5572e0609387d42c6c9fde63be035e
Tommytinny <tommyteenboi@gmail.com>
2024-12-10 19:38:49 +0100

    added files

--- utils/redis.js
+++ utils/redis.js
@@ -1,34 +1,37 @@
 const { createClient } = require('redis');
+import { promisify } from 'util';
 
 class RedisClient {
   constructor() {
     this.client = createClient();
+    this.redisStatus = false;
 
+    this.client.on('connect', () => {
+      this.redisStatus = true;
+    })
     this.client.on('error', (err) => {
       console.error('Redis connection error:', err);
     });
   }
   
   isAlive() {
-    try {
-      this.client.connect()
-      return true;
-    } catch(err) {
-      return false;
-    }
+    return this.redisStatus;
   }
   
   async get(key) {
-    const value = await this.client.get(key);
+    const getAsync = promisify(this.client.get).bind(this.client);
+    const value = await getAsync(key);
     return value;
   }
   
   async set(key, value, duration) {
-    await this.client.set(key, value, { EX: duration });
+    const setAsync = promisify(this.client.set).bind(this.client);
+    await setAsync(key, value, 'EX', duration );
   }
 
   async del(key) {
-    await this.client.del(key);
+    const delAsync = promisify(this.client.del).bind(this.client);
+    await delAsync(key);
   }
 };
 
