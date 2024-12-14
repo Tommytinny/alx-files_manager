a2e6c5101c0d724154594281b9d883aa6b3536cc
tommy <tommyteenboi@gmail.com>
2024-12-13 16:06:04 -0800

    fixed

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -6,42 +6,34 @@ const { ObjectId } = require('mongodb');
 
 class UsersController {
   static async postNew(request, response) {
-    const { email } = request.body;
-    const { password } = request.body;
-    if (!email) response.status(400).send({ error: 'Missing email' });
-    if (!password) response.status(400).send({ error: 'Missing password' });
+    const { email, password } = request.body;
+    if (!email) response.status(400).json({ error: 'Missing email' });
+    if (!password) response.status(400).json({ error: 'Missing password' });
 
-    try {
-      const users = dbClient.db.collection('users');
-      const userExist = await users.find({ email });
-      if (userExist) {
-        response.status(400).send({ error: 'Already exist' });
-      } else {
-        const hashedPassword = sha1(password);
-        const result = await users.insertOne({ email, password: hashedPassword });
-        response.status(201).send({ id: result.insertedId, email });
-      }
-    } catch (err) {
-      response.status(500).send({ error: 'Server error' });
+    await dbClient.client.connect();
+    const users = dbClient.db.collection('users');
+    const userExist = await users.find({ email });
+    if (userExist) {
+      response.status(400).json({ error: 'Already exist' });
+    } else {
+      const hashedPassword = sha1(password);
+      const result = await users.insertOne({ email, password: hashedPassword });
+      response.status(201).json({ id: result.insertedId, email });
     }
   }
 
   static async getMe(request, response) {
     const headerToken = request.headers['x-token'];
-    if (!headerToken) response.status(401).send({ error: 'Unauthorized' });
+    if (!headerToken) response.status(401).json({ error: 'Unauthorized' });
     const authKey = `auth_${headerToken}`;
     const userId = await redisClient.get(authKey);
-    if (!userId) response.status(401).send({ error: 'Unauthorized' });
-    try {
-      const users = dbClient.db.collection('users');
-      const userExist = await users.findOne({ _id: new ObjectId(userId) });
-      if (userExist) {
-        response.status(200).send({ id: userExist._id, email: userExist.email });
-      } else {
-        response.status(401).send({ error: 'Unauthorised' });
-      }
-    } catch (err) {
-      response.status(500).send({ error: 'server error' });
+    if (!userId) response.status(401).json({ error: 'Unauthorized' });
+    const users = dbClient.db.collection('users');
+    const userExist = await users.findOne({ _id: new ObjectId(userId) });
+    if (userExist) {
+      response.status(200).send({ id: userExist._id, email: userExist.email });
+    } else {
+      response.status(401).json({ error: 'Unauthorised' });
     }
   }
 }
--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import allRoutes from './routes';
+import allRoutes from './routes/index';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json());
 allRoutes(app);
 app.listen(PORT, () => {
   console.log(`Server listening on PORT ${PORT}`);
